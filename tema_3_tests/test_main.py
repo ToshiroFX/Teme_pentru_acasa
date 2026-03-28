6bc0829bec1b1d7c589fa3ea9fbefcb4141065ba
Mircea Gavrila <toshirofx1@gmail.com>
2026-03-28 13:45:31 +0200

    Fix indentation for test_chat_valid_request

--- tema_3_tests/test_main.py
+++ tema_3_tests/test_main.py
@@ -20,7 +20,7 @@ def test_root_endpoint():
     assert "message" in data or len(data) > 0
 
 #ToDo: Adăugați un scenariu de testare pentru endpoint-ul /chat/ care să fie evaluat de LLM as a Judge
-  def test_chat_valid_request():
+def test_chat_valid_request():
     payload = {
         "message": "Ce este un MSP si ce avantaje are?"
     }
